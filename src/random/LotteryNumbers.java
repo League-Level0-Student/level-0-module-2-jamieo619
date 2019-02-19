68f31d6da4e1a0202e753e4d49bbf335712f5a0a
307304 <307304@0338S-S-R0B6ZHX.sdcs.local>
2019-02-19 14:19:35 -0800

    hello

--- src/random/LotteryNumbers.java
+++ src/random/LotteryNumbers.java
@@ -1,6 +1,32 @@
 package random;
+
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
 public class LotteryNumbers {
 public static void main(String[] args) {
+	Random a = new Random ();
+	int r = a.nextInt(10);
+	
+	
+	Random b = new Random ();
+	int ra =b.nextInt(10);
+	
+	
+	Random c = new Random ();
+	int ran =c.nextInt(10);
+	
+	
+	Random d = new Random ();
+	int rand =d.nextInt(10);
+	
+	
+	Random e = new Random ();
+	int rando =e.nextInt(10);
+	
+	
+	JOptionPane.showMessageDialog(null,"Your lottery numbers are: "+ r +"-"+ ra + "-"+ran +"-"+ rand +"-" + rando);
 	
 }
 }
